bf3198c6f335e31d8dafee13782ae9fb068377ea
Guilherme Varjão Santos <guilhermmesantos98@gmail.com>
2022-05-14 10:38:22 -0300

    primeiro comit

---
+++ src/projeto01/Projeto01.java
@@ -0,0 +1,19 @@
+
+package projeto01;
+
+public class Projeto01 {
+
+    public static void main(String[] args) {
+       
+String nome; 
+nome = "Guilherme";
+
+int idade;
+idade = 23;
+
+system.out.print(nome + idade);
+
+        
+    }
+    
+}

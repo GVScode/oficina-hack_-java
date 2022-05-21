0556a51448eb06857ba94a1aa44634470c7cbd7c
Guilherme Varjão Santos <guilhermmesantos98@gmail.com>
2022-05-21 11:52:55 -0300

    projeto de entrada e saída de dados

---
+++ exercicio de fixação/src/exercicio/de/fixação/ExercicioDeFixação.java
@@ -0,0 +1,29 @@
+
+package exercicio.de.fixação;
+
+
+public class ExercicioDeFixação {
+
+   
+    public static void main(String[] args) {
+        /* Crie um programa em Java, declare duas informações de tipos diferentes e exiba a saída de dados.
+           Crie um programa que declare 4 notas, calcule a média e exiba a saída de dados.
+           Crie um programa que declare dois números e exiba as quatro operações: soma, subtração, divisão 
+           e multiplicação, usando os mesmos dois números.*/
+        
+         char guilherme;
+         String Varjão;
+         
+  
+        
+        
+        
+        
+        
+        
+                
+      
+             
+    }
+    
+}
